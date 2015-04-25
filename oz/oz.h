313f05ebcf9ec2eeadc05f4f456b61f3e3084025
Martin R. Albrecht <martinralbrecht@googlemail.com>
2015-04-25 09:13:03 +0100

    _fmpz_poly_oz_rem_small_fmpz for red of constants
    This function uses scalar multiplications instead of normal polynomial
    multiplications.

--- oz/oz.h
+++ oz/oz.h
@@ -153,6 +153,18 @@ int fmpz_poly_oz_ideal_span(const fmpz_poly_t g, const fmpz_poly_t b0, const fmp
 void _fmpz_poly_oz_rem_small(fmpz_poly_t rem, const fmpz_poly_t f, const fmpz_poly_t g, const long n, const fmpq_poly_t ginv);
 
 /**
+   @brief Return a small representative of $f \\mod \\ideal{g}$ with $f \\in \\Z$.
+
+   @param rem           return value, a small representative of $f \bmod \ideal{g}$.
+   @param f             an element $f$ in $\\Z$
+   @param g             an element $g$ in $\\R$
+   @param n             degree of cyclotomic polynomial, must be power of two
+   @param ginv          pre-computed approximate inverse of $g$ in $\\R$.
+ */
+
+void _fmpz_poly_oz_rem_small_fmpz(fmpz_poly_t rem, const fmpz_t f, const fmpz_poly_t g, const long n, const fmpq_poly_t g_inv);
+
+/**
    @brief Return a small representative of $f \\mod \\ideal{g}$.
 
    @param rem           return value, a small representative of $f \bmod \ideal{g}$.
