54be652c01dc1da2f4492954655b4544f73f653e
Daniel Wagner <dmwit@galois.com>
2016-01-15 11:21:58 -0800

    naming, one of the two hard problems in CS

--- applications/ore_dir/encrypt.c
+++ applications/ore_dir/encrypt.c
@@ -11,31 +11,31 @@ typedef struct {
 	aes_randstate_t seed;
 } encrypt_inputs;
 
-void parse_cmdline(int argc, char **argv, encrypt_inputs *const ins, location *const outs);
-void print_outputs(location outs, mife_pp_t pp, mife_ciphertext_t ct);
-void cleanup(encrypt_inputs *const ins, location *const outs);
+void parse_cmdline(int argc, char **argv, encrypt_inputs *const ins, location *const database_location);
+void print_outputs(location database, mife_pp_t pp, mife_ciphertext_t ct);
+void cleanup(encrypt_inputs *const ins, location *const database_location);
 
 int main(int argc, char **argv) {
 	encrypt_inputs ins;
-	location outs;
+	location database_location;
 	mife_ciphertext_t ct;
 
-	parse_cmdline(argc, argv, &ins, &outs);
+	parse_cmdline(argc, argv, &ins, &database_location);
 	/* TODO: keeping the whole ciphertext in memory is probably infeasible */
 	mife_encrypt(ct, &ins.pt, ins.pp, ins.sk, ins.seed);
-	print_outputs(outs, ins.pp, ct);
+	print_outputs(database_location, ins.pp, ct);
 
-	cleanup(&ins, &outs);
+	cleanup(&ins, &database_location);
 }
 
-void parse_cmdline(int argc, char **argv, encrypt_inputs *const ins, location *const outs) {
+void parse_cmdline(int argc, char **argv, encrypt_inputs *const ins, location *const database_location) {
 	/* TODO */
 }
 
-void print_outputs(location outs, mife_pp_t pp, mife_ciphertext_t ct) {
+void print_outputs(location database_location, mife_pp_t pp, mife_ciphertext_t ct) {
 	/* TODO */
 }
 
-void cleanup(encrypt_inputs *const ins, location *const outs) {
+void cleanup(encrypt_inputs *const ins, location *const database_location) {
 	/* TODO */
 }
