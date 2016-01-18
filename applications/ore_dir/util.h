f8a86a7b4f0facf59ac8d7c9b510098f94cc157a
Daniel Wagner <dmwit@galois.com>
2016-01-18 15:33:02 -0800

    CPP already implements this check

--- applications/ore_dir/util.h
+++ applications/ore_dir/util.h
@@ -3,17 +3,8 @@
 
 #include <stdbool.h>
 
-#ifdef ALLOC_FAILS
-#error "trying to redefine ALLOC_FAILS"
-#else  /* ifdef ALLOC_FAILS */
 #define ALLOC_FAILS(path, len) (NULL == ((path) = malloc((len) * sizeof(*(path)))))
-#endif /* ifdef ALLOC_FAILS */
-
-#ifdef SEED_SIZE
-#error "trying to redefine SEED_SIZE"
-#else /* ifdef SEED_SIZE */
 #define SEED_SIZE 32
-#endif /* ifdef SEED_SIZE */
 
 typedef struct {
 	char *path;
