66e5ba5b824a83868253c71f25d47323cef27760
Daniel Wagner <dmwit@galois.com>
2016-01-28 19:01:45 -0800

    formatting only

--- applications/ore_dir/parse.h
+++ applications/ore_dir/parse.h
@@ -17,21 +17,17 @@
  * int tells how many columns were in the parsed row on a success or -1 on
  * failure.
  */
-bool jsmn_parse_f2_elem  (const char *const json_string, const jsmntok_t **const json_tokens, bool      *const elem  );
-int  jsmn_parse_f2_row   (const char *const json_string, const jsmntok_t **const json_tokens, bool     **const row   , int expected_num_cols);
-bool jsmn_parse_f2_matrix(const char *const json_string, const jsmntok_t **const json_tokens, f2_matrix *const matrix);
-bool jsmn_parse_f2_mbp   (const char *const json_string, const jsmntok_t **const json_tokens, f2_mbp    *const mbp   );
-bool jsmn_parse_string   (const char *const json_string, const jsmntok_t **const json_tokens, char     **const string);
-int  jsmn_parse_string_row
-                         (const char *const json_string, const jsmntok_t **const json_tokens, char    ***const row   , int expected_num_cols);
-bool jsmn_parse_string_matrix
-                         (const char *const json_string, const jsmntok_t **const json_tokens, string_matrix
-                                                                                                        *const matrix);
-bool jsmn_parse_step     (const char *const json_string, const jsmntok_t **const json_tokens, step      *const step  );
-bool jsmn_parse_template (const char *const json_string, const jsmntok_t **const json_tokens, template  *const templ );
-bool jsmn_parse_plaintext(const char *const json_string, const jsmntok_t **const json_tokens, plaintext *const pt    );
-bool jsmn_parse_ciphertext_mapping
-                         (const char *const json_string, const jsmntok_t **const json_tokens, ciphertext_mapping *const mapping);
+bool jsmn_parse_f2_elem           (const char *const json_string, const jsmntok_t **const json_tokens, bool               *const elem    );
+int  jsmn_parse_f2_row            (const char *const json_string, const jsmntok_t **const json_tokens, bool              **const row     , int expected_num_cols);
+bool jsmn_parse_f2_matrix         (const char *const json_string, const jsmntok_t **const json_tokens, f2_matrix          *const matrix  );
+bool jsmn_parse_f2_mbp            (const char *const json_string, const jsmntok_t **const json_tokens, f2_mbp             *const mbp     );
+bool jsmn_parse_string            (const char *const json_string, const jsmntok_t **const json_tokens, char              **const string  );
+int  jsmn_parse_string_row        (const char *const json_string, const jsmntok_t **const json_tokens, char             ***const row     , int expected_num_cols);
+bool jsmn_parse_string_matrix     (const char *const json_string, const jsmntok_t **const json_tokens, string_matrix      *const matrix  );
+bool jsmn_parse_step              (const char *const json_string, const jsmntok_t **const json_tokens, step               *const step    );
+bool jsmn_parse_template          (const char *const json_string, const jsmntok_t **const json_tokens, template           *const template);
+bool jsmn_parse_plaintext         (const char *const json_string, const jsmntok_t **const json_tokens, plaintext          *const pt      );
+bool jsmn_parse_ciphertext_mapping(const char *const json_string, const jsmntok_t **const json_tokens, ciphertext_mapping *const mapping );
 
 /* top-level wrappers */
 bool jsmn_parse_template_location(const location loc, template *const template);
