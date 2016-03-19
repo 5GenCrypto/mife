449e24a9ab8427116989cefac0a33bad664d6d3e
Daniel Wagner <dmwit@galois.com>
2016-03-18 17:04:44 -0700

    delete some stupid consts

--- mife/mife_defs.h
+++ mife/mife_defs.h
@@ -19,9 +19,9 @@ typedef struct mmap_pp mmap_pp;
 /* If we call init or fread, we will call clear. In particular, we will not
  * call clear on the mmap_pp we retrieve from an mmap_sk. */
 typedef struct {
-  void (*const clear)(mmap_pp *const);
-  void (*const fread)(mmap_pp *const, FILE *const);
-  void (*const fwrite)(const mmap_pp *const, FILE *const);
+  void (*const clear)(mmap_pp *);
+  void (*const fread)(mmap_pp *, FILE *);
+  void (*const fwrite)(const mmap_pp *, FILE *);
   const size_t size;
 } mmap_pp_vtable;
 
@@ -32,31 +32,31 @@ typedef struct {
    * kappa: how many multiplications we intend to do
    * gamma: the size of the universe that we will zero-test things at
    */
-  void (*const init)(mmap_sk *const, size_t, size_t, size_t, aes_randstate_t);
-  void (*const clear)(mmap_sk *const);
-  void (*const fread)(mmap_sk *const, FILE *const);
-  void (*const fwrite)(const mmap_sk *const, FILE *const);
+  void (*const init)(mmap_sk *, size_t, size_t, size_t, aes_randstate_t);
+  void (*const clear)(mmap_sk *);
+  void (*const fread)(mmap_sk *c, FILE *);
+  void (*const fwrite)(const mmap_sk *, FILE *);
   const size_t size;
 
-  const mmap_pp *const (*const pp)(const mmap_sk *const);
-  void (*const plaintext_field)(const mmap_sk *const, fmpz_t);
+  const mmap_pp *const (*const pp)(const mmap_sk *);
+  void (*const plaintext_field)(const mmap_sk *, fmpz_t);
 } mmap_sk_vtable;
 
 typedef struct mmap_enc mmap_enc;
 
 typedef struct {
-  void (*const init)(mmap_enc *const, const mmap_pp *const);
-  void (*const clear)(mmap_enc *const);
-  void (*const fread)(mmap_enc *const, FILE *const);
-  void (*const fwrite)(const mmap_enc *const, FILE *const);
+  void (*const init)(mmap_enc *, const mmap_pp *);
+  void (*const clear)(mmap_enc *);
+  void (*const fread)(mmap_enc *, FILE *);
+  void (*const fwrite)(const mmap_enc *, FILE *);
   const size_t size;
 
-  void (*const set)(mmap_enc *const, const mmap_enc *const);
-  void (*const add)(mmap_enc *const, const mmap_pp *const, const mmap_enc *const, const mmap_enc *const);
-  void (*const mul)(mmap_enc *const, const mmap_pp *const, const mmap_enc *const, const mmap_enc *const);
-  bool (*const is_zero)(const mmap_enc *const, const mmap_pp *const);
+  void (*const set)(mmap_enc *, const mmap_enc *);
+  void (*const add)(mmap_enc *, const mmap_pp *, const mmap_enc *, const mmap_enc *);
+  void (*const mul)(mmap_enc *, const mmap_pp *, const mmap_enc *, const mmap_enc *);
+  bool (*const is_zero)(const mmap_enc *, const mmap_pp *);
   /* TODO: should this `int *` be `bool *`? */
-  void (*const encode)(mmap_enc *const, const mmap_sk *const, const fmpz_t, int *, aes_randstate_t);
+  void (*const encode)(mmap_enc *, const mmap_sk *, const fmpz_t, int *, aes_randstate_t);
 } mmap_enc_vtable;
 
 typedef struct {
