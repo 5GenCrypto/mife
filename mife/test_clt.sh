42f8fc6716b7f9508dcbc9a4975422e7c10f2ee9
Brent Carmer <bcarmer@gmail.com>
2016-04-23 10:06:29 -0700

    MIFE: tests take optional security parameter

--- mife/test_clt.sh
+++ mife/test_clt.sh
@@ -1,7 +1,7 @@
 bash test_clean.sh
 mkdir public
 cp samples/base-2-length-2-compressed-ore.json public/template.json
-./keygen -C --secparam 20
+./keygen -C --secparam ${1:-20}
 record00=`./encrypt -C '["0","00","0"]' | grep -v 'Starting\|Finished\|Generated\|Progress'`
 record11=`./encrypt -C '["1","11","1"]' | grep -v 'Starting\|Finished\|Generated\|Progress'`
 ./eval -C '{"L":"'$record00'","R":"'$record11'"}'
--- mife/test_gghlite.sh
+++ mife/test_gghlite.sh
@@ -1,7 +1,7 @@
 bash test_clean.sh
 mkdir public
 cp samples/base-2-length-2-compressed-ore.json public/template.json
-./keygen --secparam 20
+./keygen --secparam ${1:-20}
 record00=`./encrypt '["0","00","0"]' | grep -v 'Starting\|Finished\|Generated\|Progress'`
 record11=`./encrypt '["1","11","1"]' | grep -v 'Starting\|Finished\|Generated\|Progress'`
 ./eval '{"L":"'$record00'","R":"'$record11'"}'
